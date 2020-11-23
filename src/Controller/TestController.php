b9969639ffff80f89d79367558a40dd4e691b37b
Ivan Stanojevic <ivanstan@gmail.com>
2020-11-23 22:28:32 +0100

    created endpoint for testing

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\DataFixtures\ApplicationFixtures;
+use App\Entity\Application;
 use App\Entity\Submission;
 use App\EventSubscriber\MailNotificationSubscriber;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 
 /**
  * @Route("/test", condition="'dev' === '%kernel.environment%'")
@@ -21,5 +23,23 @@ class TestController extends AbstractController
     public function submission(Submission $submission): Response
     {
         return $this->render(MailNotificationSubscriber::TEMPLATE, ['submission' => $submission]);
+    }
+
+    /**
+     * @Route("/application", name="test_application")
+     */
+    public function application(): Response
+    {
+        $application = $this->getDoctrine()->getRepository(Application::class)->findBy(
+            [
+                'name' => ApplicationFixtures::APP_NAME,
+            ]
+        );
+
+        if ($application === null) {
+            throw new \RuntimeException(\sprintf('Unable to find application with name: %s', ApplicationFixtures::APP_NAME));
+        }
+
+        return $this->render('test/application.html.twig', ['application' => $application]);
     }
 }
--- src/DataFixtures/ApplicationFixtures.php
+++ src/DataFixtures/ApplicationFixtures.php
@@ -8,10 +8,12 @@ use Doctrine\Persistence\ObjectManager;
 
 class ApplicationFixtures extends Fixture
 {
+    public const APP_NAME = 'Default Application';
+
     public function load(ObjectManager $manager)
     {
         $application = (new Application())
-            ->setName('Default Application');
+            ->setName(self::APP_NAME);
 
         $manager->persist($application);
         $manager->flush();
