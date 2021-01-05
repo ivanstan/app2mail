dfbac13aacb43eec4d4264ef7ec1af93f76c5ddf
Ivan Stanojevic <ivan.stanojevic@protonmail.com>
2021-01-05 21:38:44 +0100

    test

--- maker/Resources/skeleton/crud/templates/index.tpl.php
+++ maker/Resources/skeleton/crud/templates/index.tpl.php
@@ -17,30 +17,32 @@
 <?php foreach ($entity_fields as $col => $field): ?>
                         <th><?= ucfirst($field['fieldName']) ?></th>
 <?php endforeach; ?>
-                        <th></th>
+                        <th class="text-center" style="width: 120px">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                 {% for <?= $entity_twig_var_singular ?> in pager.currentPageResults %}
                     <tr>
-
-                        <?php  dd($col) ?>
-
+<?php $i = 0 ?>
 <?php foreach ($entity_fields as $col => $field): ?>
-<?php if ($col === 0): ?>
+<?php if ($i === 0): ?>
                         <td>
                             <a href="{{ path('<?= $route_name ?>_show', {'<?= $entity_identifier ?>': <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>}) }}">
                                 {{ <?= $helper->getEntityFieldPrintCode($entity_twig_var_singular, $field) ?> }}
                             </a>
                         </td>
-    <?php else: ?>
-                        <td>{{ <?= $helper->getEntityFieldPrintCode($entity_twig_var_singular, $field) ?> }}</td>
+<?php else: ?>
+                        <td class="d-none d-sm-table-cell">{{ <?= $helper->getEntityFieldPrintCode($entity_twig_var_singular, $field) ?> }}</td>
 <?php endif; ?>
+<?php $i++ ?>
 <?php endforeach; ?>
-                        <td>
-                            <a href="{{ path('<?= $route_name ?>_edit', {'<?= $entity_identifier ?>': <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>}) }}" class="btn btn-outline-primary">
+                        <td class="d-flex justify-content-center align-baseline">
+                            <a href="{{ path('<?= $route_name ?>_edit', {'<?= $entity_identifier ?>': <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>}) }}" class="btn btn-outline-primary mr-2 edit-button">
                                 <i class="fas fa-edit" aria-hidden="true"></i>
                             </a>
+                            <div class="float-right">
+                                {{ include('<?= $route_name ?>/_delete_form.html.twig') }}
+                            </div>
                         </td>
                     </tr>
                 {% else %}
